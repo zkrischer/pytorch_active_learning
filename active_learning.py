cd96559f2670f9e33aa56af7104b391d196f93ae
Ze'ev Krischer <zkri2558@uni.sydney.edu.au>
2024-03-25 11:58:59 +1100

    Underwent the work in chapter 2 of the related textbook

--- active_learning.py
+++ active_learning.py
@@ -157,7 +157,7 @@ feature_index = {} # feature mapping for one-hot encoding
 
 def load_data(filepath, skip_already_labeled=False):
     # csv format: [ID, TEXT, LABEL, SAMPLING_STRATEGY, CONFIDENCE]
-    with open(filepath, 'r') as csvfile:
+    with open(filepath, 'r',encoding="latin-1") as csvfile:
         data = []
         reader = csv.reader(csvfile)
         for row in reader:
